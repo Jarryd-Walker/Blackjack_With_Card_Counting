275d0a7a9e0c60f28857862057e12fca1d2333c4
Jarryd-Walker <98364864+Jarryd-Walker@users.noreply.github.com>
2022-04-21 22:18:47 +1200

    Added more functionality

--- js/main.js
+++ js/main.js
@@ -1,48 +1,105 @@
+// create player objects
+
+class Participant{
+    constructor(name){
+        this.name = name
+        this.cash = 1000
+        this.wins = 0
+        this.currentBet = 0
+        this.handValue = 0
+        this.handArray = []
+    }
+
+    increaseWin(){
+        this.wins++
+    }
+    playerHandArray(newCards){
+        this.handArray = this.handArray.concat(newCards)
+        this.calculateHandValue(this.handArray)
+    }
+    calculateHandValue(cardValueArray){
+        this.handValue = cardValueArray.reduce((a,b) => a + b, 0)
+        console.log(this.handValue)
+        if(this === dealer){
+            document.querySelector(`#dealerHand`).innerText = `${this.name} hand: ${this.handValue}`
+        }else{
+            document.querySelector(`#playerHand`).innerText = `${this.name} hand: ${this.handValue}`
+        }
+    }
+    
+}
+// public, private methods???
+
+//create dealer and player for now, input later for multiple players
+const dealer = new Participant('Dealer')
+const player1 = new Participant('Player1')
+
+// get deck
+
 let deckId = ''
 
 fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-      .then(res => res.json())
-      .then(data => {
+    .then(res => res.json())
+    .then(data => {
         deckId = data.deck_id
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      })
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
 
-document.querySelector('button').addEventListener('click', startGame)
+// start game and setup function, deal 2 cards each, 1 face down for dealer
 
-// create player objects
+document.querySelector('button').addEventListener('click', deal)
+
+function deal(){
+    //set hand value to 0
+    //reset images
+    //hide Deal button
+    drawCards(1, 'dealer').then(v => dealer.playerHandArray(v))
+    drawCards(2, 'player').then(v => player1.playerHandArray(v))
+}
+
 // function to draw cards
-// start game and setup function
+
+function drawCards(numOfCards, user){
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`
+    const cardValues = fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            const container = document.querySelector(`#${user}ImgContainer`)
+            let cardValues = []
+            for(let i = 0; i < numOfCards; i++){
+                const content = document.createElement('img')
+                container.appendChild(content)
+                content.src = data.cards[i].image
+                cardValues.push(Number(cardValue(data.cards[i].value)))
+                }
+            return cardValues
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+    return cardValues
+}
+
+// calculate card value
+
+function cardValue(val){
+    if(val === 'ACE'){
+        return [11]
+    }else if (val === 'KING' || val === 'QUEEN' || val === 'JACK'){
+        return 10
+    }else{
+        return val
+    }
+}
+
 // function to hit, double down, stand, or split
 // keep hand score, dealer score, bet amount, winnings, losses, and add to local storage
 // default img for upside down card
 
 // card count logic
 
-function drawCards(numOfCards){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`
-  fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        let val = Number(cardValue(data.cards[0].value))
-        document.querySelector('#dealerImg1').src = data.cards[0].image
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
 
-function cardValue(val){
-  if(val === 'ACE' && handScore > 10){
-    return 11
-  }else if(val === 'ACE'){
-    return 1
-  }else if (val === 'KING' || val === 'QUEEN' || val === 'JACK'){
-    return 10
-  }else{
-    return val
-  }
-}
